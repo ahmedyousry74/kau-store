9062b65a583a9fdcddee578014e44dfea9a2db9b
ahmedyousry74 <ayousry750@gmail.com>
2024-07-29 17:45:25 +0300

    chekout

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,24 +25,49 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.querySelectorAll('.toggleButton').forEach(button => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         const heartIcon = this.querySelector('.heartIcon');
         heartIcon.classList.toggle('active');
     });
 });
 
 
-    //عشان تغير زرار الاكتيف على السايذ فالموديل
-    $(".size-button").click(function () {
-        $('.size-button.active').not(this).removeClass('active');
-        $(this).toggleClass('active');
-    })
+//عشان تغير زرار الاكتيف على السايذ فالموديل
+$(".size-button").click(function () {
+    $('.size-button.active').not(this).removeClass('active');
+    $(this).toggleClass('active');
+})
 
 
-    //عشان تغير زرار الاكتيف على السايذ فالموديل
-    $(".color-button").click(function () {
-        $('.color-button.active').not(this).removeClass('active');
-        $(this).toggleClass('active');
-    })
+//عشان تغير زرار الاكتيف على السايذ فالموديل
+$(".color-button").click(function () {
+    $('.color-button.active').not(this).removeClass('active');
+    $(this).toggleClass('active');
+})
 
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggleButton');
+    const categoriesList = document.getElementById('categoriesList');
+
+    // Initially hide the categories list
+    categoriesList.style.display = 'none';
+
+    // Toggle categories list on button click
+    toggleButton.addEventListener('click', function (event) {
+        event.stopPropagation();
+        if (categoriesList.style.display === 'none') {
+            categoriesList.style.display = 'block';
+        } else {
+            categoriesList.style.display = 'none';
+        }
+    });
+
+    // Hide categories list when clicking outside
+    document.addEventListener('click', function (event) {
+        if (!categoriesList.contains(event.target) && !toggleButton.contains(event.target)) {
+            categoriesList.style.display = 'none';
+        }
+    });
+});
